1e88de909ee092814d406322fcabc69bdfae18e5
ahmadashraf604 <ahmadashraf604@gmail.com>
2019-02-16 18:32:45 +0200

    add database helper class edit on DAO product

--- src/main/java/com/ecommerce/daos/DaoProduct.java
+++ src/main/java/com/ecommerce/daos/DaoProduct.java
@@ -7,7 +7,9 @@ package com.ecommerce.daos;
 
 import com.ecommerce.beans.Product;
 import com.ecommerce.utilities.DataBaseConnection;
+import com.ecommerce.utilities.DatabaseHelper;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -22,17 +24,11 @@ import java.util.logging.Logger;
  */
 public class DaoProduct {
 
-    Connection connection;
-
-    public DaoProduct() {
-        DataBaseConnection dataBaseConnection = DataBaseConnection.getInstance();
-        connection = dataBaseConnection.getConnection();
-        System.out.println("connection is done");
-    }
-
     public List<Product> getAllProduct() {
         List<Product> products = new ArrayList<>();
         try {
+            DataBaseConnection dataBaseConnection = DataBaseConnection.getInstance();
+            Connection connection = dataBaseConnection.getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCTS");
             while (resultSet.next()) {
@@ -45,10 +41,94 @@ public class DaoProduct {
                 product.setQuantity(resultSet.getInt("QUANTITY"));
                 products.add(product);
             }
+//            dataBaseConnection.close();
         } catch (SQLException ex) {
-            System.out.println("jmdf,.gsk/sdkfj");
             Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, null, ex);
         }
         return products;
+    }
+
+//    public int addProduct(Product product){
+//        Product product = new Product();
+//        try {
+//            DataBaseConnection dataBaseConnection = DataBaseConnection.getInstance();
+//            Connection connection = dataBaseConnection.getConnection();
+//            Statement statement = connection.createStatement();
+//            statement.executeUpdate("INSERT INTO PRODUCTS");
+//            
+//                product.setProductId(resultSet.getInt("PRODUCT_ID"));
+//                product.setName(resultSet.getString("PRODUCT_NAME"));
+//                product.setPrice(resultSet.getInt("PRICE"));
+//                product.setDiscount(resultSet.getDouble("DISCOUNT"));
+//                product.setCategoryId(resultSet.getInt("CATEGORY_ID"));
+//                product.setQuantity(resultSet.getInt("QUANTITY"));
+//           
+//            dataBaseConnection.close();
+//        } catch (SQLException ex) {
+//            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return product;
+//    }
+    public List<Product> getProduct(int categoryId) {
+        List<Product> products = new ArrayList<>();
+        try {
+            DataBaseConnection dataBaseConnection = DataBaseConnection.getInstance();
+            Connection connection = dataBaseConnection.getConnection();
+            String sql
+                    = "SELECT * FROM " + DatabaseHelper.PRODUCT.TABLE_NAME
+                    + "where " + DatabaseHelper.PRODUCT.CATEGORY_ID + " = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, categoryId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Product product = new Product();
+                product.setProductId(resultSet.getInt("PRODUCT_ID"));
+                product.setName(resultSet.getString("PRODUCT_NAME"));
+                product.setPrice(resultSet.getInt("PRICE"));
+                product.setDiscount(resultSet.getDouble("DISCOUNT"));
+                product.setCategoryId(resultSet.getInt("CATEGORY_ID"));
+                product.setQuantity(resultSet.getInt("QUANTITY"));
+                products.add(product);
+            }
+            dataBaseConnection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return products;
+    }
+
+    public List<Product> getProduct(String productName) {
+        List<Product> products = new ArrayList<>();
+        try {
+            DataBaseConnection dataBaseConnection = DataBaseConnection.getInstance();
+            Connection connection = dataBaseConnection.getConnection();
+            String sql
+                    = "SELECT * FROM " + DatabaseHelper.PRODUCT.TABLE_NAME
+                    + "where " + DatabaseHelper.PRODUCT.NAME + " = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "%" + productName + "%");
+            setOnList(preparedStatement.executeQuery(), products);
+            dataBaseConnection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return products;
+    }
+
+    private void setOnList(ResultSet resultSet, List<Product> products) {
+        try {
+            while (resultSet.next()) {
+                Product product = new Product();
+                product.setProductId(resultSet.getInt("PRODUCT_ID"));
+                product.setName(resultSet.getString("PRODUCT_NAME"));
+                product.setPrice(resultSet.getInt("PRICE"));
+                product.setDiscount(resultSet.getDouble("DISCOUNT"));
+                product.setCategoryId(resultSet.getInt("CATEGORY_ID"));
+                product.setQuantity(resultSet.getInt("QUANTITY"));
+                products.add(product);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoProduct.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
