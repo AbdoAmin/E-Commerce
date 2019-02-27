17076805a3d3a6f694e55a7f824b553cd4c1da17
AbdoAmin <abdelrahman.amin01@gmail.com>
2019-02-27 12:35:22 +0200

    Interesting.jsp & serv & Dao

--- src/main/java/filters/LogOut.java
+++ src/main/java/filters/LogOut.java
@@ -9,6 +9,7 @@ import com.ecommerce.beans.User;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -37,6 +38,14 @@ public class LogOut implements Filter {
             ((HttpServletResponse) response).sendRedirect("/E-Commerce/index.jsp");
 
         }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
     }
 
    
